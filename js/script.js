874db203fdb5377f84d464db2916f56ce90b60f5
Camila Coder <camilanierocosta@hotmail.com>
2018-12-17 17:57:47 -0500

    missing form functionallity

--- js/script.js
+++ js/script.js
@@ -113,27 +113,62 @@ $('a[href*="#"]')
     }
   });
 
+  
+
   $(document).ready(function() {
-    $("#submit").click(function() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-    $("#returnmessage").empty(); // To empty previous error/success message.
-    // Checking for blank fields.
-    if (name == '' || email == '' || message == '') {
-    alert("Please Fill Required Fields");
-    } else {
-    // Returns successful data submission message when the entered information is stored in database.
-    $.post("contact_form.php", {
-    name1: name,
-    email1: email,
-    message1: message,
-    }, function(data) {
-    $("#returnmessage").append(data); // Append returned message to message paragraph.
-    if (data == "Your Query has been received, I will contact you soon.") {
-    $("#form")[0].reset(); // To reset form fields on success.
+
+    var form = $('#form'),
+        email = $('#email'),
+        subject = $('#subject'),
+        message = $('#message'),
+        info = $('#info'),
+        submit = $("#submit");
+    
+    form.on('input', '#email, #subject, #message', function() {
+      $(this).css('border-color', '');
+      info.html('').slideUp();
+    });
+    
+    submit.on('click', function(e) {
+      e.preventDefault();
+      if(validate()) {
+        $.ajax({
+          type: "POST",
+          url: "mailer.php",
+          crossDomain: true,
+          data: form.serialize(),
+          dataType: "json"
+        }).done(function(data) {
+          if(data.success) {
+            email.val('');
+            subject.val('');
+            message.val('');
+            info.html('Message sent!').css('color', 'green').slideDown();
+          } else {
+            info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
+          }
+        });
+      }
+    });
+    
+    function validate() {
+      var valid = true;
+      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      
+      if(!regex.test(email.val())) {
+        email.css('border-color', 'red');
+        valid = false;
+      }
+      if($.trim(subject.val()) === "") {
+        subject.css('border-color', 'red');
+        valid = false;
+      }
+      if($.trim(message.val()) === "") {
+        message.css('border-color', 'red');
+        valid = false;
+      }
+      
+      return valid;
     }
-    });
-    }
-    });
-    });
+  
+  });
