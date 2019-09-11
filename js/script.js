4fe412503c2ac7054e9093be866117ee57b300a6
cahcnc91 <camilanierocosta@hotmail.com>
2019-09-11 09:24:58 -0400

    4 boxes and popup done

--- js/script.js
+++ js/script.js
@@ -76,48 +76,34 @@ window.onload = function() {
 })();
 
 // Select all links with hashes
-// $('a[href*="#"]')
-//   // Remove links that don't actually link to anything
-//   .not('[href="#"]')
-//   .not('[href="#0"]')
-//   .click(function(event) {
-//     if (event.currentTarget.hash !== "#popup") {
-//       // On-page links
-//       if (
-//         location.pathname.replace(/^\//, "") ==
-//           this.pathname.replace(/^\//, "") &&
-//         location.hostname == this.hostname
-//       ) {
-//         // Figure out element to scroll to
-//         location.replace(`${location.hostname}/${event.currentTarget.hash}`);
-//         var target = $(this.hash);
-//         target = target.length
-//           ? target
-//           : $("[name=" + this.hash.slice(1) + "]");
-//         // Does a scroll target exist?
-//         if (target.length) {
-//           // Only prevent default if animation is actually gonna happen
-//           event.preventDefault();
-//           $("html, body").animate(
-//             {
-//               scrollTop: target.offset().top
-//             },
-//             1000,
-//             function() {
-//               // Callback after animation
-//               // Must change focus!
-//               var $target = $(target);
-//               $target.focus();
-//               if ($target.is(":focus")) {
-//                 // Checking if the target was focused
-//                 return false;
-//               } else {
-//                 $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-//                 $target.focus(); // Set focus again
-//               }
-//             }
-//           );
-//         }
-//       }
-//     }
-//   });
+$('a[href*="#"]').click(function(event) {
+  if (
+    event.currentTarget.hash !== "#popup-thinkempire" ||
+    event.currentTarget.hash !== "#popup-wikipedia" ||
+    event.currentTarget.hash !== "#popup-grocery" ||
+    event.currentTarget.hash !== "#popup-puppydate" ||
+    event.currentTarget.hash !== "#projects"
+  ) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        // event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+      }
+    }
+  }
+});
